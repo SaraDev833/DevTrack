25dabb5a28a893927972623befb4d4e20fe68f9f
Sheikh Sara <sksara706@gmail.com>
2026-07-13 17:06:05 +0600

    added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Dashboard/Navbar'
 import Kanban from '../components/Kanban'
+import TaskOverViewChart from '../components/Dashboard/TaskOverViewChart'
 
 const Dashboard = () => {
   return (
     <div className='w-full bg-slate-100'>
 <Navbar/>
 <Kanban/>
+<div className='flex justify-between '>
+  <div className='w-1/2 my-6'>
+     <TaskOverViewChart/>
+  </div>
+
+</div>
     </div>
   )
 }
