ba1e9857c6507c8efd0459593bcd1a852b0857a3
Sheikh Sara <sksara706@gmail.com>
2026-08-11 18:37:31 +0600

    added

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -1,8 +1,9 @@
 import { Camera, X } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import man1 from "../../assets/man-1.jfif"
 import { useForm } from "react-hook-form"
 const EditProfile = ({user,setEditProfileModal}) => {
+    const [previewImage, setPreviewImage] = useState(user.avatar);
      const {
     register,
     handleSubmit,
@@ -11,7 +12,16 @@ const EditProfile = ({user,setEditProfileModal}) => {
   } = useForm()
 
   const onSubmit = (data) => console.log(data)
-
+const handleImageChange=(e)=>{
+  
+    const file = e.target.files[0];
+  
+    if(file){
+        const imgUrl = URL.createObjectURL(file);
+      
+        setPreviewImage(imgUrl)
+    }
+}
   console.log(watch("example"))
   return (
     <div className='fixed inset-0 z-50 bg-black/50 backdrop:blur-sm flex items-center justify-center p-4 h-auto'>
@@ -23,16 +33,59 @@ const EditProfile = ({user,setEditProfileModal}) => {
  <form onSubmit={handleSubmit(onSubmit)}>
             <div className='my-4  flex items-center justify-center flex-col'>
                 <div className='relative'>
-     <img src={man1} className='h-30 w-30 object-cover object-top rounded-full' alt="" />
-     <div  className='absolute right-2 bottom-1.5 text-white p-2 rounded-full bg-indigo-600 border-2 border-white'>
-         <Camera size={14}/>
-     </div>
+     <img src={previewImage} className='h-30 w-30 object-cover object-top rounded-full' alt="" />
+   <label
+  htmlFor="profileImage"
+  className="
+  absolute right-2 bottom-1.5
+  text-white p-2
+  rounded-full
+  bg-indigo-600
+  border-2 border-white
+  cursor-pointer
+  "
+>
+  <Camera size={14}/>
+</label>
+<input
+  type="file"
+  accept="image/*"
+  id="profileImage"
+  className="hidden"
+  onChange={handleImageChange}
+/>
      </div>
      <p className='text-sm font-medium text-nowrap my-2'>Click the icon to change photo</p>
   </div>
-      <input defaultValue="test" {...register("example")} />
-
-      {/* include validation with required or other standard HTML validation rules */}
+  <div className='flex flex-col w-full gap-2'>
+  <label htmlFor="name" className='font-medium text-lg'>Full Name</label>
+      <input type='text' className='rounded-md border outline-none border-indigo-600 focus:ring-2 focus:ring-2-indigo-600 p-2 text-sm font-medium' value={user.name}  {...register("name", {
+        required: true
+      })} />
+         {errors.name && <span>This field is required</span>}
+</div>
+  <div className='flex flex-col w-full gap-2'>
+  <label htmlFor="email" className='font-medium text-lg'>Email</label>
+      <input type='text' className='rounded-md border outline-none border-indigo-600 focus:ring-2 focus:ring-2-indigo-600 p-2 text-sm font-medium' value={user.email}  {...register("email", {
+        required: true
+      })} />
+         {errors.name && <span>This field is required</span>}
+</div>
+  <div className='flex flex-col w-full gap-2'>
+  <label htmlFor="name" className='font-medium text-lg'>Full Name</label>
+      <input type='text' className='rounded-md border outline-none border-indigo-600 focus:ring-2 focus:ring-2-indigo-600 p-2 text-sm font-medium' value={user.name}  {...register("name", {
+        required: true
+      })} />
+         {errors.name && <span>This field is required</span>}
+</div>
+  <div className='flex flex-col w-full gap-2'>
+  <label htmlFor="name" className='font-medium text-lg'>Full Name</label>
+      <input type='text' className='rounded-md border outline-none border-indigo-600 focus:ring-2 focus:ring-2-indigo-600 p-2 text-sm font-medium' value={user.name}  {...register("name", {
+        required: true
+      })} />
+         {errors.name && <span>This field is required</span>}
+</div>
+      
       <input {...register("exampleRequired", { required: true })} />
       {/* errors will return when field validation fails  */}
       {errors.exampleRequired && <span>This field is required</span>}
